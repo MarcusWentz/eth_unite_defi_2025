1a7d5bbda353e4ff7334d83a61a229c6dd0f9b36
marcus <markzcollege404@gmail.com>
2025-07-27 19:11:09 -0400

    test_timelock_set_deployed_at_mask_value_1 bytes

--- cross-chain-swap/contracts/cross-chain-swap/src/test.rs
+++ cross-chain-swap/contracts/cross-chain-swap/src/test.rs
@@ -7,7 +7,7 @@ use crate::escrow_factory::timelocks::{
     TimelocksClient,
     Stage
 };
-use soroban_sdk::{vec, BytesN, Env, U256, symbol_short};
+use soroban_sdk::{vec, BytesN, Env, U256, symbol_short, Bytes};
 
 #[test]
 fn test_merkle_storage_invalidator() {
@@ -302,13 +302,45 @@ fn test_timelock_set_deployed_at_mask_value_0() {
     let timelock_input_u256 : U256 = U256::from_u32(&env, 100);
     let mask_value_input_u256 : U256 = U256::from_u32(&env, 0);
 
-    let test_return_value : U256 = client.set_deployed_at(&timelock_input_u256, &mask_value_input_u256);
+    let test_return_value_256 : U256 = client.set_deployed_at(&timelock_input_u256, &mask_value_input_u256);
 
     // Expect to return 1 as type uin256.
     assert_eq!(
-        test_return_value, 
+        test_return_value_256, 
         U256::from_u32(&env, 100)
     );
 
 }
 
+#[test]
+fn test_timelock_set_deployed_at_mask_value_1() {
+    let env = Env::default();
+    let contract_id = env.register(Timelocks, ());
+    let client = TimelocksClient::new(&env, &contract_id);
+
+    let timelock_input_u256 : U256 = U256::from_u32(&env, 1);
+    let mask_value_input_u256 : U256 = U256::from_u32(&env, 1);
+
+    let test_return_value_256 : U256 = client.set_deployed_at(&timelock_input_u256, &mask_value_input_u256);
+   
+    let test__return_value_bytes : Bytes = test_return_value_256.to_be_bytes();
+
+    // Create a new Bytes array which we can modify.
+    let mut bytes_array : Bytes = Bytes::new(&env);
+
+    // // Modify Bytes array to be uin256 max value.
+    // bytes_array.extend_from_array(&[0xFFu8; 32]);
+
+    // Modify bytes array to match output.
+    // In decimal format:
+    // set_deployed_at(1,1) from the 1inch contract in Solidity is:
+    // 26959946667150639794667015087019630673637144422540572481103610249217
+    bytes_array.extend_from_array(&[ 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, ]);
+
+    // Expect to return 1 as type uin256.
+    assert_eq!(
+        test__return_value_bytes, 
+        bytes_array
+    );
+
+}
