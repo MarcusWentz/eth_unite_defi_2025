99f85dc8c0501ac03134480fea7db7f5a9ce1abc
marcus <markzcollege404@gmail.com>
2025-08-01 15:39:43 -0400

    TakerTraitsBuilder new test compiles

--- cross-chain-swap/contracts/order/src/taker_traits_test.rs
+++ cross-chain-swap/contracts/order/src/taker_traits_test.rs
@@ -250,17 +250,18 @@ fn create_test_address(env: &Env) -> Address {
 
 #[test]
 fn test_unwrap_weth() {
-    // let env = create_test_env();
 
-    // // Test default (no unwrap)
-    // let traits_default = TakerTraitsBuilder::new(env.clone()).build();
+    let env = create_test_env();
+
+    // Test default (no unwrap)
+    let traits_default = TakerTraitsBuilder::new(env.clone()).build();
 
     // assert!(!TakerTraitsLib::unwrap_weth(env.clone(), traits_default));
 
-    // // Test with unwrap WETH flag set
-    // let traits_unwrap = TakerTraitsBuilder::new(env.clone()).unwrap_weth().build();
+    //     // Test with unwrap WETH flag set
+    //     let traits_unwrap = TakerTraitsBuilder::new(env.clone()).unwrap_weth().build();
 
-    // assert!(TakerTraitsLib::unwrap_weth(env.clone(), traits_unwrap));
+    //     assert!(TakerTraitsLib::unwrap_weth(env.clone(), traits_unwrap));
 }
 
 // #[test]
