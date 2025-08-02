8c5a92cfdd33f5c5b5654a37e0fbc02d4943c4d4
marcus <markzcollege404@gmail.com>
2025-08-02 07:40:02 -0400

    test_create_dst_escrow start test input setup

--- cross-chain-swap/contracts/escrow/src/test_escrow_factory.rs
+++ cross-chain-swap/contracts/escrow/src/test_escrow_factory.rs
@@ -59,3 +59,39 @@ fn test_store_escrow_wasm_escrowType_destination() {
     client.store_escrow_wasm(&bytes_32_max, &escrow_type_status);
 }
 
+#[test]
+fn test_create_dst_escrow() {
+
+    let env = Env::default();    
+    let contract_id = env.register(EscrowFactory, ());
+    let client = EscrowFactoryClient::new(&env, &contract_id);
+
+    // 1893477661 unix time is the start of year 2030
+    let input_src_cancellation_timestamp : U256 = U256::from_u32(&env, 1893477661);
+    
+    //Define immutables struct values.
+    let address_input_test : Address = Address::from_str(&env, "CCJNI7JJQF23TO3PVBIN3V4R66EWBD3AFNQ6EL4POPSXHZT4IYXIQ5KI");
+    let u256_input_test : U256 = U256::from_u32(&env, 1);
+    let u128_input_test : u128 = 1;
+    let raw_value_max_uint256_bytes = [0xFFu8; 32]; // 32 bytes of 0xFF
+    let bytes_32_max: BytesN<32> = BytesN::from_array(&env, &raw_value_max_uint256_bytes);
+    let input_immutables : Immutables = Immutables { 
+        order_hash: bytes_32_max.clone(), 
+        hashlock: bytes_32_max.clone(), 
+        maker: address_input_test.clone(), 
+        taker: address_input_test.clone(), 
+        token: address_input_test.clone(), 
+        amount: u128_input_test, 
+        safety_deposit: u128_input_test, 
+        timelocks: u256_input_test 
+    };
+    
+    // // Test create_dst_escrow with return address from function call.
+    // let test_address_return_output : Address = client.create_dst_escrow(
+    //     &input_immutables, 
+    //     &input_src_cancellation_timestamp);
+
+    // let output_address : Address = Address::from_str(&env, "CCJNI7JJQF23TO3PVBIN3V4R66EWBD3AFNQ6EL4POPSXHZT4IYXIQ5KI");
+    // assert_eq!(test_address_return_output, output_address);
+
+}
